307fa7f6470b8a4365ce72523a290d7f288ea655
USER <106021016@live.asia.edu.tw>
2017-09-30 15:59:32 +0800

    HW FINISH

---
+++ src/公升轉美加侖.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class 公升轉美加侖 {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int c =scn.nextInt();
+        System.out.println(Math.round((c*0.26418)));
+    }
+}
